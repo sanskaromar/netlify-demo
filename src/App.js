5010b46f4db1af830cc30f222edf0776bb99e1ea
Shruti Varshney <shrutivarshney1424@gmail.com>
2023-04-25 01:34:43 +0530

    added pdf viewer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import PDFViewer from './components/PDFViewer';
+
 function App() {
   return (
     <div className="bg-emerald-300 h-screen flex flex-col justify-center items-center">
       <p className="font-bold underline text-4xl p-24 text-yellow-950"> Netlify Demo</p>
+      <PDFViewer />
     </div>
   );
 }
